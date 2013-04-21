ae86427d5514c7f5da119f46de185d7f6dd0ccc1
Keita Ishibashi <fromjpn@gmail.com>
2013-04-21 22:04:06 +0900

    add processingjs sample

--- sandbox/myscript.js
+++
@@ -1,9 +0,0 @@
-var mainCtrl = function($scope) {
-    $scope.users = [
-        {"name":"taguchi", "score":10},
-        {"name":"tanaka", "score":30},
-        {"name":"yamada", "score":20},
-        {"name":"yamadan", "score":40},
-        ];
-    $scope.today = new Date();
-}
